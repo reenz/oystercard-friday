42800abdaf7d11f699cb3508f3feae64c5d507d0
Phil <phil.makers@outlook.com>
2018-01-08 15:40:45 +0000

    Step 4 complete - Balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
+require 'oystercard'
 describe Oystercard do
-
+  subject :oystercard  { described_class.new }
+  it "check oystercard balance" do
+    expect(oystercard.balance).to eq 0
+  end
 
 end
