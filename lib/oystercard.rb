f9b2221f02fb9431ca4ad35800f0553dba653e1a
samuelmaker <samuelwood.co@gmail.com>
2018-01-09 14:53:57 +0000

    8: touch in and out functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize
     @balance = 0.00
+    @journey_status = false
   end
 
   def top_up(amount)
@@ -18,8 +19,20 @@ class Oystercard
     @balance -= amount
   end
 
+  def touch_in
+    @journey_status = true
+  end
+
+  def touch_out
+    @journey_status = false
+  end
+
+  def in_journey?
+    @journey_status
+  end
+
   private
-  
+
   def maximum_exceeded?(amount)
     @balance + amount > BALANCE_LIMIT
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,36 +1,57 @@
 require 'oystercard'
 
 describe Oystercard do
-  subject :oystercard  { described_class.new }
+  subject :card  { described_class.new }
 
   it "balance should be zero" do
-    expect(oystercard.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
   describe '#top_up' do
 
     it "top up adds to balance" do
-      expect { oystercard.top_up(2) } .to change { oystercard.balance } .by 2
+      expect { card.top_up(2) } .to change { card.balance } .by 2
     end
 
     it "top up will not exceed balance limit" do
       max_balance = Oystercard::BALANCE_LIMIT
       amount = Oystercard::BALANCE_LIMIT + 1
       error = "£#{amount} top up failed, balance will exceed £#{max_balance}"
-      expect { oystercard.top_up(amount) } .to raise_error error
+      expect { card.top_up(amount) } .to raise_error error
     end
 
   end
 
   describe '#deduct' do
     before do
-      oystercard.top_up(10)
+      card.top_up(10)
     end
 
     it "should deduct amount from balance" do
-      expect { oystercard.deduct(3) } .to change { oystercard.balance } .by -3
+      expect { card.deduct(3) } .to change { card.balance } .by -3
     end
 
+  end
+
+  describe '#in_journey?' do
+
+    it 'returns false before any touches' do
+      expect( card ).to_not be_in_journey
+    end
+
+  end
+
+  describe '#touch_in' do
+    it 'changes oystercard journey status to true' do
+      expect { card.touch_in } .to change { card.in_journey? } .to true
+    end
+  end
+
+  describe '#touch_out' do
+    it 'changes oystercard journey status to false' do
+      card.touch_in
+      expect { card.touch_out } .to change { card.in_journey? } .to false
+    end
   end
 
 end
